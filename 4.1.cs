70dc377cbafbcc2cd573f53537f28379df925bf6
JadeWongyoungyoo <nawaf.w1@gmail.com>
2021-04-22 22:23:17 +0700

    งาน4.1

---
+++ 4.1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace งาน_4._1
+{
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+            int row = int.Parse(Console.ReadLine());
+            while(row < 0)
+            {
+                Console.WriteLine("Invalid Pascal’s triangle row number.");
+                row = int.Parse(Console.ReadLine()); 
+            }
+            Pascal(row);
+        }
+        static void Pascal(int rows)
+        {
+            int  val = 1, i, j;
+            Console.WriteLine("Pascal's triangle");
+            for (i = 0; i <= rows; i++)
+            {                
+                for (j = 0; j <= i; j++)
+                {
+                    if (j == 0 || i == 0)
+                        val = 1;
+                    else
+                        val = val * (i - j + 1) / j ;
+                    Console.Write(val + " ");
+                }
+                Console.WriteLine();  
+            }                         
+                          
+        }
+    }
+}
